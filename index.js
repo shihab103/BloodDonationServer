a2b138858408de36d9547a4f9f75d9a90f653caa
Shihab Uddin <shihabuddin2469@gmail.com>
2026-01-31 01:16:35 +0600

    create backend environment

---
+++ index.js
@@ -0,0 +1,48 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const { MongoClient } = require("mongodb");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// MongoDB setup
+const client = new MongoClient(process.env.MONGO_URI);
+
+async function connectDB() {
+  try {
+    await client.connect();
+    console.log("Connected to MongoDB");
+
+    const db = client.db("software");
+    const collection = db.collection("testCollection");
+
+
+
+
+
+
+
+
+
+  } catch (err) {
+    console.error("❌ MongoDB connection error:", err);
+  }
+}
+
+// Call connectDB
+connectDB();
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Hello Express!");
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`🚀 Server is running on http://localhost:${PORT}`);
+});
